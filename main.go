df4515b328baedce66f2638bb7c2eb81c811d863
Laurent Demailly <ldemailly@gmail.com>
2024-08-18 12:39:38 -0700

    Harden the service, use the memory limit from grol 0.51+ to avoid fatal/crashes, limit length of values in state file (#80)
    * Harden the service
    
    Per discord gophers reco
    
    * Note about using LoadCredential= and limit memory usage to avoid fatal errors
    
    * Prep for using grol-io/grol#159 (will edit this PR when released)
    
    * released version of grol

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"flag"
+	"math"
 	"os"
+	"runtime/debug"
 
 	"fortio.org/log"
 	"fortio.org/scli"
 )
 
-var depth = flag.Int("max-depth", 10000, "Maximum depth of recursion")
+var (
+	depth  = flag.Int("max-depth", 10000, "Maximum depth of recursion")
+	maxLen = flag.Int("max-save-len", 1000, "Maximum len of saved identifiers, use 0 for unlimited")
+)
 
 func main() {
 	num := flag.Int("n", 100, "Maximum number of messages to keep in memory for possible edit")
@@ -18,5 +23,9 @@ func main() {
 		log.Fatalf("DISCORD_BOT_TOKEN must be set")
 	}
 	AutoLoadSave = !(os.Getenv("GROL_DISABLE_AUTOSAVE") == "1")
+	memlimit := debug.SetMemoryLimit(-1)
+	if memlimit == math.MaxInt64 {
+		log.Fatalf("Memory limit not set, please set GOMEMLIMIT=1GiB or similar")
+	}
 	Run(*num)
 }
