5872ec5b2d76eb1d7e282d01ba3810b05399324c
Laurent Demailly <ldemailly@gmail.com>
2024-08-30 16:41:31 -0700

    Use upcoming MaxDuration from grol (#104)
    * Use upcoming MaxDuration from grol
    
    * use now released version

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"math"
 	"os"
 	"runtime/debug"
+	"time"
 
 	"fortio.org/log"
 	"fortio.org/scli"
@@ -16,6 +17,7 @@ var (
 	libraryCode string
 	depth       = flag.Int("max-depth", 2500, "Maximum depth of recursion")
 	maxLen      = flag.Int("max-save-len", 2000, "Maximum len of saved identifiers, use 0 for unlimited")
+	maxDur      = flag.Duration("max-duration", 3*time.Second, "Maximum duration of scripts, use 0 for unlimited")
 	panicF      = flag.Bool("panic", false, "Don't catch panic (DEV only)")
 )
 
