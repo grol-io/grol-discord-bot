d32fbeb6ab0b4f8997e53ab1582c3b8c413e9242
Laurent Demailly <ldemailly@gmail.com>
2025-05-02 18:21:39 -0700

    text version playing

--- main.go
+++ main.go
@@ -16,9 +16,10 @@ var (
 	//go:embed discord.gr
 	libraryCode string
 	depth       = flag.Int("max-depth", 2500, "Maximum depth of recursion")
-	maxLen      = flag.Int("max-save-len", 2000, "Maximum len of saved identifiers, use 0 for unlimited")
-	maxDur      = flag.Duration("max-duration", 3*time.Second, "Maximum duration of scripts, use 0 for unlimited")
-	panicF      = flag.Bool("panic", false, "Don't catch panic (DEV only)")
+	// bjs is big (3900+ bytes)
+	maxLen = flag.Int("max-save-len", 4000, "Maximum len of saved identifiers, use 0 for unlimited")
+	maxDur = flag.Duration("max-duration", 3*time.Second, "Maximum duration of scripts, use 0 for unlimited")
+	panicF = flag.Bool("panic", false, "Don't catch panic (DEV only)")
 )
 
 func main() {
