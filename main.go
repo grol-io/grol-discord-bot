1bd35eb7ab5d89e0244a38a91da80a24fc47901c
Laurent Demailly <ldemailly@gmail.com>
2025-05-05 14:14:37 -0700

    update comment

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ var (
 	//go:embed discord.gr
 	libraryCode string
 	depth       = flag.Int("max-depth", 2500, "Maximum depth of recursion")
-	// bjs is big (3900+ bytes).
+	// bjs map is big (4300+ bytes with 1 shuffled deck).
 	maxLen = flag.Int("max-save-len", 4500, "Maximum len of saved identifiers, use 0 for unlimited")
 	maxDur = flag.Duration("max-duration", 3*time.Second, "Maximum duration of scripts, use 0 for unlimited")
 	panicF = flag.Bool("panic", false, "Don't catch panic (DEV only)")
