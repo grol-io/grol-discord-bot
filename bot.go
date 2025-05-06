3ed2b0cd23cd18f7a8ca5a98187f535a6d4c1f13
Laurent Demailly <ldemailly@gmail.com>
2025-05-06 12:47:11 -0700

    fix english tense

--- bot.go
+++ bot.go
@@ -258,7 +258,7 @@ func evalInput(input string, p *CommandParams) string {
 		res = "💡 Grol bot help: grol bot evaluates [grol](<https://grol.io>) language fragments, as simple as expressions like `1+1`" +
 			" and as complex as defining closures, using map, arrays, etc... the syntax is similar to go (without needing " +
 			"`:=`, plain `=` is enough). Use `info` to see all functions, keywords, etc..." +
-			" Try `TicTacToe()` or `Butterfly()` or `Blackjack()` for more advanced examples that includes grol" +
+			" Try `TicTacToe()` or `Butterfly()` or `Blackjack()` for more advanced examples that include grol" +
 			" handling discord interactions and complex messages.\n\n" +
 			"Either in DM or @grol or with `!grol` prefix (or `!grol -f` for also showing formatted code, `-c` in compact mode," +
 			" `-d` debug expressions)" +
